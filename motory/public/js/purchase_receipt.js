cae5b8919223d96ed56cf772c19301da9dca7901
Shahzad Naser <shahzadnaser1122@gmail.com>
2023-07-19 22:48:06 +0500

    update purchase receipt for number plate

--- motory/public/js/purchase_receipt.js
+++ motory/public/js/purchase_receipt.js
@@ -4,22 +4,51 @@ var targetRoot;
 var maState;
 
 frappe.ui.form.on("Purchase Receipt", {
-	onload_post_render: function(frm) {
-    $(frm.fields_dict['car_structure_html'].wrapper)
-    .html('<div  style="position: relative; display: flex;flex-direction: column;align-items: center;justify-content: center;padding-top: 50px;"> \
-    <img  id="sourceImage"   src="/assets/motory/image/car_structure.png" style="max-width: 900px; max-height: 80%;"  crossorigin="anonymous" /> \
-    <img  id="sampleImage"   src="/assets/motory/image/car_structure.png"  style="max-width: 900px; max-height: 100%; position: absolute;" crossorigin="anonymous" /> \
-    </div>');
+  setup:function(frm){
 
-    setSourceImage(document.getElementById("sourceImage"));
+    frm.set_query('letters', 'items', (frm,cdt,cdn) => {
+      return {
+          filters: {
+              "is_numeric":0
+          }
+      }            
+    });
+    frm.set_query('numbers', 'items', (frm,cdt,cdn) => {
+      return {
+          filters: {
+              "is_numeric":1
+          }
+      }            
+    });
+  },
+	// onload_post_render: function(frm) {
+  //   $(frm.fields_dict['car_structure_html'].wrapper)
+  //   .html('<div  style="position: relative; display: flex;flex-direction: column;align-items: center;justify-content: center;padding-top: 50px;"> \
+  //   <img  id="sourceImage"   src="/assets/motory/image/car_structure.png" style="max-width: 900px; max-height: 80%;"  crossorigin="anonymous" /> \
+  //   <img  id="sampleImage"   src="/assets/motory/image/car_structure.png"  style="max-width: 900px; max-height: 100%; position: absolute;" crossorigin="anonymous" /> \
+  //   </div>');
 
-    const sampleImage = document.getElementById("sampleImage");
-    sampleImage.addEventListener("click", () => {
-      showMarkerArea(sampleImage);
-    });      
+  //   setSourceImage(document.getElementById("sourceImage"));
+
+  //   const sampleImage = document.getElementById("sampleImage");
+  //   sampleImage.addEventListener("click", () => {
+  //     showMarkerArea(sampleImage);
+  //   });      
+  // }
+	refresh: function(frm) {
+	}
+});
+
+frappe.ui.form.on("Purchase Receipt Item", {
+  numbers:function(frm,cdt,cdn){
+    var item = frappe.get_doc(cdt, cdn);
+    frappe.model.set_value(cdt, cdn, "car_plate_no_cf", (item.letters || '') + '-' + (item.numbers || ''));
+  },
+  letters:function(frm,cdt,cdn){
+    var item = frappe.get_doc(cdt, cdn);
+    frappe.model.set_value(cdt, cdn, "car_plate_no_cf", (item.letters || '') + '-' + (item.numbers || ''));
   }
-})
-
+});
 function setSourceImage(source) {
   sourceImage = source;
   targetRoot = source.parentElement;
@@ -47,3 +76,5 @@ function showMarkerArea(target) {
     markerArea.restoreState(JSON.parse(cur_frm.doc.car_structure_annotation));
   }
 }
+
+
