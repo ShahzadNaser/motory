97fcd335cd448caca982cba0b8f860982c23cf65
Raja Abdullah Arshad <rajaabdullaharshad@gmail.com>
2023-10-01 11:14:38 +0500

    print formats Je Pe

--- motory/hooks.py
+++ motory/hooks.py
@@ -227,6 +227,20 @@ user_data_fields = [
 	}
 ]
 
+fixtures = [
+    {
+        "dt": "Custom Field",
+        "filters": [
+            ["name", "in", [
+                # journal Entry Account
+                "Journal Entry Account-account_name_ar",
+				# Payment Entry Deduction
+			  	"Payment Entry Deduction-account_name_ar"
+
+            ]
+            ]]
+    },
+    ]
 # Authentication and authorization
 # --------------------------------
 
