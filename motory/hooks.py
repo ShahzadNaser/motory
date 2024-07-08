d655cee15feee1600b55fc1815ae9342cd677e42
Shahzad Naser <shahzadnaser1122@gmail.com>
2024-07-08 10:11:56 +0500

    update hooks

--- motory/hooks.py
+++ motory/hooks.py
@@ -118,9 +118,12 @@ doc_events = {
 		"on_update_after_submit" :  "motory.api.sync_accessories_inspection_details",
 		"on_cancel": "motory.api.update_car_status"
 		},
+	"Purchase Order":{
+		"before_save":"motory.api.add_car_plate_no"
+	},
 	"Purchase Receipt": { 
 		"before_validate":"motory.api.copy_car_serial_to_vin",
-		"validate": ["motory.api.fetch_accessories_inspection_details","motory.api.validate_single_serial_no"],
+		"validate": ["motory.api.fetch_accessories_inspection_details","motory.api.validate_single_serial_no","motory.api.add_car_plate_no"],
 		"on_submit": ["motory.api.update_car_status","motory.api.copy_car_fields_to_serial_no_doc"],
 		"on_update_after_submit": "motory.api.sync_accessories_inspection_details",
 		"on_cancel": "motory.api.update_car_status"
