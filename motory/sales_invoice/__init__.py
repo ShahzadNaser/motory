3766d41ee3822fc6d52e18c6ea910a0bed01be69
Shahzad Naser <shahzadnaser1122@gmail.com>
2024-10-23 10:36:58 +0000

    update

--- motory/sales_invoice/__init__.py
+++ motory/sales_invoice/__init__.py
@@ -25,6 +25,7 @@ def add(invoice=None):
         si.company = frappe.defaults.get_global_default("company")
         si.posting_date = getdate(params.get("invoice_date"))
         si.due_date = getdate(params.get("invoice_date"))
+        si.supply_date_cf = getdate(params.get("invoice_date"))
         si.update_stock = 0
         si.set_posting_time = 1
         si.reference_id = params.get("reference_id")
